3551738f827b372ee799893a5dfa73fad04d3344
Alexey <protolicious@hotmail.com>
2023-09-16 16:55:53 +0300

    f

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
+import static java.lang.Math.divideExact;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+
+        Scanner enterTheNumber = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        int enteredNumberByUser = enterTheNumber.nextInt();
+
+
+       if ((enteredNumberByUser % 3 == 0) && (enteredNumberByUser % 5 == 0))  {
+            System.out.println("Divisible by 3 AND 5");
+        }
+
+       else if (enteredNumberByUser % 3 == 0) {
+            System.out.println("Divisible by 3");
+        }
+
+       else if (enteredNumberByUser % 5 == 0) {
+           System.out.println("Divisible by 5");
+       }
+
+        else {
+            System.out.println("Not Divisible by 3 nor 5");
+        }
+
+
+
+        }
+
+
+
+
+
     }
-}
+
